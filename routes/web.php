95a19ab0d61aadd00a656d2f4f850b905cebb206
yuto.ohta <yuto-ota@dac.co.jp>
2023-06-16 01:16:15 +0000

    follow, unfollow

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MicropostsController;
+use App\Http\Controllers\UserFollowController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,7 +29,14 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::group(['middleware' => ['auth']], function () {                                    // 追記
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['prefix' => 'users/{id}'], function () {                                          // 追記
+        Route::post('follow', [UserFollowController::class, 'store'])->name('user.follow');         // 追記
+        Route::delete('unfollow', [UserFollowController::class, 'destroy'])->name('user.unfollow'); // 追記
+        Route::get('followings', [UsersController::class, 'followings'])->name('users.followings'); // 追記
+        Route::get('followers', [UsersController::class, 'followers'])->name('users.followers');    // 追記
+    });
+    
     Route::resource('users', UsersController::class, ['only' => ['index', 'show']]);
     Route::resource('microposts', MicropostsController::class, ['only' => ['store', 'destroy']]);
 });
